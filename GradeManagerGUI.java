ea2851caf3948c3e4026a36142054b0daaacfc01
KeyaanSchool <keyaan.wali2022@gmail.com>
2026-06-06 18:11:45 -0400

    Setting up textfields, labels, buttons

--- GradeManagerGUI.java
+++ GradeManagerGUI.java
@@ -5,4 +5,8 @@ import javax.swing.*;
 // Defining the main class for the Grade Manager GUI application.
 public class GradeManagerGUI extends JFrame {
 
+    private JTextField[] markFields = new JTextField[10]; // Array to hold text fields for user input for marks (up to 10 courses)
+    private JLabel resultLabel; // Label to display calculated letter grade (only overall average)
+    private JButton calculateButton, clearButton; // Buttons for calculating average grades and clearing inputs
+
 }
