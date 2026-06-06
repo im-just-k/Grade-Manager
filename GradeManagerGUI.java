b2847f5acde59ba50ae3e5604cb18fcc2c330980
KeyaanSchool <keyaan.wali2022@gmail.com>
2026-06-06 18:02:17 -0400

    Add GradeManagerGUI.java

---
+++ GradeManagerGUI.java
@@ -0,0 +1,8 @@
+// Importing all necessary libraries for GUI components and event handling first.
+import javax.swing.*;
+
+
+// Defining the main class for the Grade Manager GUI application.
+public class GradeManagerGUI extends JFrame {
+
+}
